8d3c7911ffbedc2ce574c9e83ec4a4d765238fc0
mehmetkanpolat <mehmet.kanpolat@gkfx.com>
2018-02-28 18:18:54 +0300

    StubManager complex type issue while caching the item

--- src/StubMiddleware.Core/Caching/DefaultStubTypeCacheKeyGenerator.cs
+++ src/StubMiddleware.Core/Caching/DefaultStubTypeCacheKeyGenerator.cs
@@ -5,6 +5,10 @@
         public string GenerateKey<T>()
         {
             var refType = typeof(T);
+            if (refType.IsGenericType)
+            {
+                return $"{refType.Assembly.GetName().Name}_{refType.GetGenericTypeDefinition().FullName}";
+            }
             return $"{refType.Assembly.GetName().Name}_{refType.FullName}";
         }
     }
